8362260c0bfff9e94eb394cdfc75b12a7eec1abf
hpfield <59520303+hpfield@users.noreply.github.com>
2022-08-17 09:49:54 +0100

    edit programme tutorials

--- PGT/Programme-Tutorials/tb1/au.sh
+++ PGT/Programme-Tutorials/tb1/au.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-
-if [ "$#" != "2" ]
-then
-    echo provide filename to assemble and port
-    exit 1
-fi
-
-if arm-none-eabi-as -o "$1".o -g "$1".s
-then
-    if arm-none-eabi-ld -o "$1" "$1".o
-    then
-        if qemu-arm -singlestep -g "$2" "$1" &
-        then
-            if gdb-multiarch -q
-            then
-                echo success   
-            fi
-        fi
-    fi
-else
-    echo failure
-    exit 1
-fi
-
+# e = fail script if any command fails
+# u = fail script if reference to any undefined var
+set -eu
+filename="$1"
+port="$2"
+arm-none-eabi-as -o "$filename".o -g "$filename".s
+arm-none-eabi-ld -o "$filename" "$filename".o
+qemu-arm -singlestep -g "$port" "$filename" &
+gdb-multiarch -iex "file $filename" -iex "target
+remote localhost:$port" -q
